632ee59a527164bce47eee14d0759de78ab9e047
afsanchezsa <30636303+afsanchezsa@users.noreply.github.com>
2021-03-11 04:02:40 +0000

    Deploying to gh-pages from  @ d1ae46a461a47ac5a7da8c8c16b5e764727fdc84 🚀

---
+++ docs/sketches/negative.js
@@ -0,0 +1,30 @@
+var myp5 = new p5((p) => {
+    
+  
+    p.setup = function() {
+        p.createCanvas(710, 400);
+        // here we use a callback to display the image after loading
+        p.loadImage('../sketches/lenna.png', img => {
+            let width=500;
+            let d = p.pixelDensity();
+            let pink=p.color(230,176,200);
+          p.image(img, 0, 0,width,width);
+          //p.filter(p.INVERT);
+          p.loadPixels();
+        
+          
+          for (let i = 0; i < p.pixels.length; i += 4) {
+            p.pixels[i] = p.red(255-p.pixels[i]);
+            p.pixels[i + 1] = p.green(255-p.pixels[i+1]);
+            p.pixels[i + 2] = p.blue(255-p.pixels[i+2]);
+            p.pixels[i + 3] = p.alpha(255-p.pixels[i+3]);
+          }
+          p.updatePixels();
+        });
+        
+      }
+  
+   
+  
+  
+  }, "negative");
