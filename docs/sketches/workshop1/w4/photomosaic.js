faf964db3efacd24770a3f9e976df35ab3d71ff4
EC2 Default User <ec2-user@ip-172-31-71-159.ec2.internal>
2021-04-03 05:01:00 +0000

    photomosaic explanation text

--- docs/sketches/workshop1/w4/photomosaic.js
+++ docs/sketches/workshop1/w4/photomosaic.js
@@ -1,69 +1,68 @@
 let picture;
-let sp;
 let w_scaled;
 let h_scaled;
-let dataset= [];
 let availableColors;
-const scaleFactor = 8
-const datasetSize =105
-let im = []
+let dataset= [];
+let loadedImages = [];
+const scaleFactor = 8;
+const datasetSize =105;
 
 function preload() {
-    let location = '../sketches/workshop1/w4/regular_show.jpg'
+    const location = '../sketches/workshop1/w4/regular_show.jpg'
     picture = loadImage(location);
-    resizeCanvas(picture.width,picture.height)
     loadStrings('../sketches/workshop1/w4/availableColors.txt',loadDataset)
-    loadDataset();
+    //loadDataset();
     noLoop()
 }
+
 
 function setup() {
     createCanvas(600, 600);
     noLoop();
 }
 
+
 function draw() {
     w_scaled = Math.floor(picture.width / scaleFactor);
     h_scaled = Math.floor(picture.height / scaleFactor);
-    picture.resize(w_scaled,h_scaled)
-    picture.loadPixels()
-    image(picture, 0, 0);
+    picture.resize(w_scaled,h_scaled);
+    picture.loadPixels();
     for(let x = 0; x < w_scaled; x++) {
-      for(let y = 0; y < h_scaled; y++) {
-        let [r, g, b] = picture.get(x, y);
-          const choice = closestColor(r,g,b)
-          const pixelImage = im[choice]
-          image(pixelImage,x*scaleFactor,y*scaleFactor)
-          noLoop()
-      }
+        for(let y = 0; y < h_scaled; y++) {
+            const [r, g, b] = picture.get(x, y);
+            const index = closestColor(r,g,b);
+            const pixelImage = loadedImages[index];
+            image(pixelImage,x*scaleFactor,y*scaleFactor);
+        }
     }
-    noLoop();
 }
+
 
 function closestColor(r,g,b) {
     let minDistance = -1;
-    let choice;
+    let index;
     for (let i=0; i < datasetSize; ++i) {
-        const color_i = dataset[i];
-        let distance = dist(r,g,b,color_i[0],color_i[1],color_i[2]);
-        if (minDistance==-1 || distance < minDistance) {
+        const img_i = dataset[i];
+        let distance = dist(r,g,b,img_i[0],img_i[1],img_i[2]);
+        if (minDistance == -1 || distance < minDistance) {
             minDistance = distance;
-            choice = i;
+            index = i;
         }
     }
-    return choice;
+    noLoop();
+    return index;
 }
 
 function loadDataset(availableColors){
     availableColors.slice(0,datasetSize).map(c => {
-        const r = parseInt(c.substring(0,3),10)
-        const g = parseInt(c.substring(4,7),10)
-        const b = parseInt(c.substring(8,11),10)
-        dataset.push([r,g,b])
+        const r = parseInt(c.substring(0,3),10);
+        const g = parseInt(c.substring(4,7),10);
+        const b = parseInt(c.substring(8,11),10);
+        dataset.push([r,g,b]);
         loadImage(`../sketches/workshop1/w4/dataset/${c}`, il => { 
-            il.resize(scaleFactor,scaleFactor)
-            im.push(il)
+            il.resize(scaleFactor,scaleFactor);
+            loadedImages.push(il);
         })
     })
-    noLoop()
+    noLoop();
 }
