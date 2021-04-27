edd2b23f0de487b4474c8c4cd35bf5e75a7f6774
unknown <afsanchezsa@unal.edu.co>
2021-04-27 09:58:07 -0500

    class shaders

--- docs/sketches/textureShader.js
+++ docs/sketches/textureShader.js
@@ -1,46 +1,24 @@
 
- // this variable will hold our shader object
- let theShader;
- // this variable will hold our createGraphics layer
- let img;
- function preload(){
-   // load the shader
-   theShader = loadShader('../sketches/shaders/shader.vert','../sketches/shaders/texture.frag');
-   img=loadImage('../sketches/lenna.png');
- }
-
- function setup() {
-   // shaders require WEBGL mode to work
-   createCanvas(710, 400, WEBGL);
-   perspective(PI / 3.0, width / height, 0.1, 500);
-   //ortho(-width/2,width/2,-height/2,height/2);
-   // initialize the createGraphics layers
-
-
-   // turn off the createGraphics layers stroke
-    noStroke();
-    textureMode(NORMAL);
-  
-
-
- }
-
- function draw() {
-    background(0);
-
-    //rotateX(frameCount * 0.01);
-    //rotateY(frameCount * 0.01);
-    beginShape()
-    vertex(-width / 2, -height / 2, 0, 0, 0);
-    vertex(width / 2, -height / 2, 0, 1, 0);
-    vertex(width / 2, height / 2, 0, 1, 1);
-    vertex(-width / 2, height / 2, 0, 0, 1);
-    endShape(CLOSE)
-    //texture (img);
-    theShader.setUniform('texture',img);
-    shader(theShader);
-    orbitControl();
-    //box(300);
- 
- }
+let theShader;
+let img;
+function preload() {
+  theShader = loadShader('../sketches/shaders/shader.vert', '../sketches/shaders/texture.frag');
+  img = loadImage('../sketches/lenna.png');
+}
+function setup() {
+  createCanvas(710, 400, WEBGL);
+  noStroke();
+  textureMode(NORMAL);
+  shader(theShader);
+  theShader.setUniform('texture', img);
+}
+function draw() {
+  background(0);
+  beginShape()
+  vertex(-width / 2, -height / 2, 0, 0, 0);
+  vertex(width / 2, -height / 2, 0, 1, 0);
+  vertex(width / 2, height / 2, 0, 1, 1);
+  vertex(-width / 2, height / 2, 0, 0, 1);
+  endShape(CLOSE)
+}
 // good example in https://editor.p5js.org/cocopon/sketches/rke1-X8t7
